0980a5d64e016ecb3cd19dbe343d3df793d5dbb9
David <david.corticchiato@gmail.com>
2014-04-06 16:48:19 +0200

    Hello World with titanium-element

--- main.dart
+++ main.dart
@@ -1,2 +1,11 @@
+import 'package:titanium-element/el.dart';
+
 void main() {
+  el('window')
+    .attr('backgroundColor', 'white')
+    .append(
+      el('label')
+        .attr('text', 'Hello World !')
+    )
+    .open();
 }
